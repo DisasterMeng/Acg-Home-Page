788b62700c472a0316f71164c8ff2a719b579445
Hens <Hens@tembin.com>
2019-07-03 00:25:20 +0800

    update:acg item

--- src/components/AcgItem.vue
+++ src/components/AcgItem.vue
@@ -1,28 +1,59 @@
 <template lang="pug">
-    div.acg-item
+    div.acg-item(@click='itemHandler(acg.link)')
+        img(:src='acg.src', )
         p {{acg.title}}
-    
+
 </template>
 
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+    import {Vue, Component, Prop} from 'vue-property-decorator'
 
 
-export  interface AcgItemBean {
-	src: string,
-	title: string,
-	link: string
-}
+    export interface AcgItemBean {
+        src: string,
+        title: string,
+        link: string
+    }
 
 
-@Component
-export default class AcgItem extends Vue {
+    @Component
+    export default class AcgItem extends Vue {
 
-	@Prop({type: Object as () => AcgItemBean})
-	private acg!: AcgItemBean
+        @Prop({type: Object as () => AcgItemBean})
+        private acg!: AcgItemBean
 
 
+        private itemHandler(link: string) {
+            window.location.href = link
+        }
 
-}
+    }
 </script>
+
+<style lang="stylus" scoped>
+    .acg-item
+        display flex
+        flex-direction column
+        margin 16px
+        height 80px
+        width 80px
+        cursor pointer
+        align-items center
+        border white solid 1px
+        background-color white
+        border-radius 80px
+
+        &:hover
+            border rgba(0, 0, 0, 0.1) solid 1px
+
+
+        img
+            height 40px
+            width 40px
+            border-radius 40px
+
+        p
+            font-size 15px
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,76 @@
 <template lang='pug'>
-	div.home-container
-		search
-		div.acg-group-container
-			div(v-for="acg in acgs", :key="acg.link")
-				acg-item(:acg='acg')
-			
+    div.home-container
+        search
+        div.acg-group-container
+            acg-item.acg-item(v-for="acg in acgs", :key="acg.link", :acg='acg')
+
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+    import {Component, Vue} from 'vue-property-decorator'
 
-import Search from '../components/Search.vue'
-import AcgItem, { AcgItemBean } from '../components/AcgItem.vue'
+    import Search from '../components/Search.vue'
+    import AcgItem, {AcgItemBean} from '../components/AcgItem.vue'
 
 
+    @Component({
+        components: {
+            Search,
+            AcgItem
+        }
+    })
+    export default class Home extends Vue {
 
-@Component({
-	components: {
-		Search,
-		AcgItem
-	}
-})
-export default class Home extends Vue {
-
-	private acgs: AcgItemBean[] = [
-		{
-			title: 'title',
-			link: 'linek',
-			src: 'src'
-		}
-	]
-}
+        private acgs: AcgItemBean[] = [
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            },
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            },
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            },
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            },
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            },
+            {
+                title: 'title',
+                link: 'https://www.baidu.com',
+                src: 'https://www.baidu.com/img/bd_logo1.png'
+            }
+        ]
+    }
 </script>
 
 <style lang="stylus" scoped>
-.home-container
-	height 100%
-	width 100%
-	display flex
-	justify-content center
-	align-items center
+    .home-container
+        height 100%
+        width 100%
+        display flex
+        justify-content center
+        align-items center
+        flex-direction column
+
+        .acg-group-container
+            margin-top 16px
+            display flex
+            flex-wrap wrap
+            max-width 600px
+
+
 </style>
 
